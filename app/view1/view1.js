bd51bcacd873a1631caa429cf3eb83799c5e7a1e
Shruti Khattar <saketkumar@Sakets-MacBook-Air.local>
2016-11-27 00:59:36 +0100

    Installing some components and creating login page

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -1,14 +1,17 @@
 'use strict';
 
 angular.module('myApp.view1', ['ngRoute'])
+    .config(['$routeProvider', function ($routeProvider) {
+        $routeProvider.when('/view1', {
+            templateUrl: 'view1/view1.html',
+            controller: 'View1Ctrl'
+        });
+    }])
 
-.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view1', {
-    templateUrl: 'view1/view1.html',
-    controller: 'View1Ctrl'
-  });
-}])
-
-.controller('View1Ctrl', [function() {
-
-}]);
+    .controller('View1Ctrl', ['$scope', '$filter', '$alert', function ($scope, $filter, $alert) {
+        console.log('In View1');
+        $scope.username = '';
+        $scope.login = function () {
+            console.log('In login method');
+        }
+    }]);
